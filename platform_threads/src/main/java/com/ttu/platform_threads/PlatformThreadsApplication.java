75219bc35a162b38bd481279b1ba841bb6d3a2bd
Ariellus-Antoro <ariellus.antoro022@gmail.com>
2026-09-01 20:52:51 +0700

    initial commit

---
+++ platform_threads/src/main/java/com/ttu/platform_threads/PlatformThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.ttu.platform_threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformThreadsApplication.class, args);
+	}
+
+}
---
+++ platform_threads/src/test/java/com/ttu/platform_threads/PlatformThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttu.platform_threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
